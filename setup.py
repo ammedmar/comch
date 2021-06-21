3e01f6755fa0703c273ab80411551de3c5477fab
ammedmar <ammedmar@gmail.com>
2021-06-21 12:26:54 +0200

    Version 0.1.4

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="comch",
-    version="0.1.3",
+    version="0.1.4",
     author="Anibal M. Medina-Mardones",
     author_email="ammedmar@gmail.com",
-    description="A package to study commutativity up to coherent homotopies",
+    description="A specialized computer algebra system for the study of commutativity up to coherent homotopies",
     long_description=long_description,
     long_description_content_type="text/markdown",
     #url="https://github.com/ammedmar/comch",
